f147badf4ded81d17450a18d31e32569bcc77e50
Kelley Skinner <skinner677@gmail.com>
2023-11-02 12:59:07 -0400

    add my image

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,20 +11,12 @@ import { Box, HStack } from "@chakra-ui/react";
 
 const socials = [
   {
-    icon: faEnvelope,
-    url: "mailto: hello@example.com",
-  },
-  {
     icon: faGithub,
     url: "https://github.com",
   },
   {
     icon: faLinkedin,
     url: "https://www.linkedin.com",
-  },
-  {
-    icon: faMedium,
-    url: "https://medium.com",
   },
   {
     icon: faStackOverflow,
--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import portrait from "../images/photo-of-me.jpg"
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Kelley!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -12,14 +13,14 @@ const LandingSection = () => (
   <FullScreenSection
     justifyContent="center"
     alignItems="center"
-    isDarkBackground
-    backgroundColor="#2A4365"
+    backgroundColor="white"
+    color="#18181b"
   >
     <VStack>
-      <Avatar size='5xl' name='Pete' src="https://i.pravatar.cc/150?img=7" />
+      <Avatar size='2xl' name='Kelley' src={portrait} />
       <Heading as='h4'size='md' paddingBottom='20px'>{greeting}</Heading>
-      <Heading as='h1' size='3xl'>{bio1}</Heading>
-      <Heading as='h1' size='3xl'>{bio2}</Heading>
+      <Heading as='h1' size='2xl'>{bio1}</Heading>
+      <Heading as='h1' size='2xl'>{bio2}</Heading>
     </VStack>
   </FullScreenSection>
 );
